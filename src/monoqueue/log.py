c3984965045048a107a53db571173336ed5a374f
Curtis Rueden <ctrueden@wisc.edu>
2023-06-23 15:23:21 -0500

    Attach log level accessors to singleton log object
    Hacky but effective!

--- src/monoqueue/log.py
+++ src/monoqueue/log.py
@@ -14,6 +14,11 @@ The monoqueue logger.
 import logging, os
 
 log = logging.getLogger("monoqueue")
+log.is_critical = lambda: log.isEnabledFor(logging.CRITICAL)
+log.is_error = lambda: log.isEnabledFor(logging.ERROR)
+log.is_warning = lambda: log.isEnabledFor(logging.WARNING)
+log.is_info = lambda: log.isEnabledFor(logging.INFO)
+log.is_debug = lambda: log.isEnabledFor(logging.DEBUG)
 
 def setup_logging():
     level = (
