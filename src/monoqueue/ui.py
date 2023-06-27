94a19641204479f1ac9133f846d34fed39ac213f
Curtis Rueden <ctrueden@wisc.edu>
2023-06-26 21:33:47 -0500

    Display GitHub issue body below the impact report
    For now, we hardcode item['issue']['body'], because the firefox and
    discourse handlers do not currently deliver any body-like text.
    If that changes in the future, we can generalize the body field to
    be handler-agnostic, and unhardcode from the curses UI.
    
    The body is lightly processed to wrap intelligently at word boundaries.
    The string writing routine in general is enhanced to put ellipses when
    asked to write strings that are too long for the screen width.
    
    New key handlers allow scrolling the body, when there are too many lines
    to fit on the screen at once: body scan be scrolled one line at a time
    with J/K or the up/down arrows, or scrolled half a screen at a time with
    the PgUp/PgDn keys. There is currently a small bug in that PgUp/PgDn
    always scroll 1/2 the entire screen height, rather than 1/2 the body
    viewport height, but I'm too lazy to address it for the moment.
    
    Finally, the curses UI is more resilient to window resizing.
    Previously, it would occasionally crash when resized to smaller widths.
    Unfortunately, the curses.COLS and curses.LINES constants do not change
    when the window is resized; there is probably a way to detect and
    address resizes, but I did not yet investigate how to do it.

--- src/monoqueue/ui.py
+++ src/monoqueue/ui.py
@@ -31,6 +31,12 @@ C_TITLE = 4
 C_DATES = 5
 C_SCORE = 6
 
+K_UP = (75, 69, 89, 95, 85, 80)
+K_DN = (75, 69, 89, 95, 68, 79, 87, 78)
+K_LT = (75, 69, 89, 95, 76, 69, 70, 84)
+K_RT = (75, 69, 89, 95, 82, 73, 71, 72, 84)
+K_PGUP = (75, 69, 89, 95, 80, 80, 65, 71, 69)
+K_PGDN = (75, 69, 89, 95, 78, 80, 65, 71, 69)
 
 class UI:
     def __init__(self, stdscr):
@@ -40,6 +46,7 @@ class UI:
         self.url = None
         self.index = None
         self.count = None
+        self.scroll_offset = None
         self.jump(0)
 
     def refresh(self):
@@ -53,7 +60,7 @@ class UI:
         updated = item["updated"]
         impact = self.mq.impact(self.url)
         issue = item.get("issue")
-        body = issue.get("body") if issue else None
+        body = UI.split_lines(issue.get("body"), 0) if issue else None
 
         row = -1
         self.write(self.url, (row := row + 1), c=C_URL)
@@ -83,7 +90,13 @@ class UI:
         # Body, if available
         row = max(l_row, r_row)
         self.draw_divider(row := row + 1)
-        # if body: self.write(f"***{body}***", (row := row + 1), 0)
+
+        if body:
+            max_offset = len(body) - curses.LINES + row + 1
+            self.scroll_offset = max(0, min(self.scroll_offset, max_offset))
+            for i in range(self.scroll_offset, len(body)):
+                self.write(body[i], (row := row + 1), 0)
+                if row >= curses.LINES: break
 
         # Cursor prompt
         self.write("> ", prompt_row, 0)
@@ -97,18 +110,69 @@ class UI:
     def draw_divider(self, y):
         self.write("-" * curses.COLS, y, 0)
 
+    @staticmethod
+    def trunc_line(line, max_len):
+        if len(line) <= max_len: return line
+        end = max_len - 3
+        return "" if end < 0 else line[:end] + "..."
+
+    @staticmethod
+    def split_lines(s, x):
+        """
+        Split up a multi-line message block, intelligently wrapping at words.
+        """
+        lines = []
+        if s is None: return lines
+
+        max_len = curses.COLS - x
+        code = False
+        for line in re.split("\r\n|\n", s.replace("\t", "    ")):
+            if line.startswith("```"): code = not code
+            tack = True
+            while len(line) > max_len:
+                if code:
+                    # No line splitting inside a code fence.
+                    end = len(line)
+                    tack = False
+                else:
+                    # Split up long lines on words.
+                    end = line[:max_len].rfind(" ")
+                    if end < 0: end = line.find(" ", max_len)
+                    if end < 0: end = len(line)
+
+                lines.append(line[:end])
+                line = line[end + 1:]
+
+            if tack: lines.append(line)
+
+        return lines
+
     def write(self, msg, y=None, x=None, c=0):
         s = str(msg)
-        if y is not None and x is None:
-            # center the string, truncating with ellipses if needed
-            if len(s) > curses.COLS:
-                s = s[:curses.COLS-3] + "..."
-            x = max(0, (curses.COLS - len(s)) // 2)
-        if x is None and y is None:
+        if y is None:
+            # Draw the string at the current cursor position.
             self.stdscr.addstr(s, curses.color_pair(c))
-        else:
-            # TODO: bounds check coords vs screen size
+            return
+
+        if x is None:
+            # Center the string, truncating with ellipses if needed.
+            s = UI.trunc_line(s, curses.COLS)
+            x = (curses.COLS - len(s)) // 2
+
+        if x >= curses.COLS or y >= curses.LINES:
+            # String is out of bounds; draw nothing.
+            return
+
+        if x < 0 or y < 0:
+            raise ValueError(f"Invalid coordinates: {y=}, {x=}")
+
+        s = UI.trunc_line(s, curses.COLS - x)
+        try:
             self.stdscr.addstr(y, x, s, curses.color_pair(c))
+        except:
+            # Every so often, curses crashes while resizing the terminal.
+            # Ignoring these errors seems to behave just fine, so we do.
+            pass
 
     def quit(self, exit_code=0):
         sys.exit(exit_code)
@@ -120,11 +184,16 @@ class UI:
         return key
 
     def do_operation(self, key: str):
+        code = tuple(ord(c) for c in key)
         if key in "oO": webbrowser.open(self.url)
         elif key in "nN": self.jump(self.index + 1)
         elif key in "pP": self.jump(self.index - 1)
         elif key in "qQ": self.quit()
         elif key in "123456789": self.defer(ord(key) - ord('0'))
+        elif key in "jJ" or code == K_DN: self.scroll(1)
+        elif key in "kK" or code == K_UP: self.scroll(-1)
+        elif code == K_PGDN: self.scroll(curses.LINES // 2)
+        elif code == K_PGUP: self.scroll(-curses.LINES // 2)
         return True
 
     def loop(self):
@@ -145,6 +214,7 @@ class UI:
         self.count = len(urls)
         self.index = max(0, min(index, self.count - 1))
         self.url = urls[self.index]
+        self.scroll_offset = 0
 
     def defer(self, days):
         self.mq.defer(self.url, time.days_later(days))
@@ -155,6 +225,9 @@ class UI:
         # Refresh the display.
         self.jump(self.index)
 
+    def scroll(self, amount):
+        self.scroll_offset += amount
+        if self.scroll_offset < 0: self.scroll_offset = 0
 
 def main(*args):
     curses.wrapper(lambda stdscr: UI(stdscr).loop())
